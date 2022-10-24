9c9c42d0979593ff5446c83844b05d40ad1028d1
Jorge Peña <jandres-0818@hotmail.com>
2022-10-24 01:24:21 -0500

    comentario de requerimients 2

--- task/models.py
+++ task/models.py
@@ -1,7 +1,3 @@
-from email.policy import default
-from http.client import ImproperConnectionState
-from tkinter import CASCADE
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth.models import User
 
